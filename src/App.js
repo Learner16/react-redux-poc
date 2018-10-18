08a3364e22f5932dcaa2716fa7590f5cd1e68429
Rachna Adwani <Rachna Adwani>
2018-10-18 21:45:51 +0530

    Updated the readme file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import WebCamera from './webCamera/index';
 
 const App = () => (
   <div className="App">
-    <p> Task1: The state of the button is </p>
+    <p>
+      Task1: The state of the button is handled by redux. Current state:
+    </p>
     <CustomButton />
     <p> Task 2: Access the web Camera: </p>
     <WebCamera />
